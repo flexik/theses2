46dfc2f8aafe2e3e66502e500646038a50139b90
Tibor Szolár <git@tibor.szolar.cz>
2016-11-23 11:57:04 +0100

    Configure default route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :faculties
   resources :theses
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'theses#index'
 end
