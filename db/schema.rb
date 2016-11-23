9068f6de6026cf437c279f10a4ffe4c13b28304f
Tibor Szolár <git@tibor.szolar.cz>
2016-11-23 11:51:23 +0100

    Add faculties

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123103212) do
+ActiveRecord::Schema.define(version: 20161123104533) do
+
+  create_table "faculties", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "theses", force: :cascade do |t|
     t.string   "title"
